4dac54c565ae623760cc4e3221d381cad3eff493
hellak1014 <108450086+hellak1014@users.noreply.github.com>
2022-08-05 18:06:48 +0900

    basic

---
+++ basic/src/basic/ex1.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 7;
+		
+			if (a>b) {
+				System.out.println(a);
+			} else {
+				System.out.println(b);
+			}
+
+	}
+
+}
---
+++ basic/src/basic/ex2.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class ex2 {
+	public static void main(String[] args) {
+		
+		
+		int a = 10;
+		int b = 7;
+		int t = 0;
+		
+		t = a;
+		a = b;
+		b = t;
+		
+		System.out.print("a :" + a +", b :" + b);
+		
+		
+	}
+}
